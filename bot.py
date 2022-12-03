169fb0d11768cb8842104eb7160b4787fae376fc
Cagri Tasci <cagri.c.tasci@outlook.de>
2022-12-03 17:16:45 +0000

    basic bot

---
+++ bot.py
@@ -0,0 +1,71 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import random
+import logging
+import os
+import datetime 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', 80))
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+HEROKUURL = os.getenv('HEROKU_URL')
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+responses = [
+    "Ich weiß dass dir folgendes gefallen wird ",
+    "Dein heutiges Geschenk: ",
+    "Herzlichen Glückwunsch, du erhälst ",
+    "Zur Feier des heutigen Tages schenke ich dir ",
+    "Frohlocke! Heute gibts ",
+    "Its dangerous to go alone. Take this: ",
+    "Welch herzhafter Tag für ein herzhaftes Stück ",
+    "Gepriesen sei der ",
+    "Von mir, für dich ",
+    "Ach wie schön. Es ist ",
+    "Ach toll! Heute gibt es ",
+    "Wie schön wäre jetzt ein Stück ",
+    "Von ganzem Herzem ",
+    "Was du schon immer wolltest ",
+    "Was wäre jetzt besser als "
+    "Du warst artig dieses Jahr! du erhälst "
+    "Warst du etwa doch unartig? denn heute gib es "
+    ""
+]
+
+item = "Knoblauch!"
+
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    # Send a message when the command /start is issued
+    update.message.reply_text('Hallo! ich habe einen einzigartigen Adventskalender für dich gebastelt')
+
+
+def tuer(update, context):
+    
+    day = datetime.datetime.today().day
+    if day > len(responses):
+        return
+    response = responses[day] + "..." + item
+    # send message
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+# linking the /random command with the function random() 
+day_handler = CommandHandler('tuer', tuer)
+dispatcher.add_handler(day_handler)
+
+
+updater.start_webhook(
+        listen="0.0.0.0",
+        port=int(PORT),
+        url_path=TOKEN,
+        webhook_url='' + TOKEN
+    )
+
+updater.idle()
