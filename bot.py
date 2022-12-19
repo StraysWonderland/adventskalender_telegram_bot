cacd742d3e0fe3a7e4bcafb3c6752871d04ab953
Cagri Tasci <cagri.c.tasci@outlook.de>
2022-12-19 23:48:24 +0000

    test function added

--- bot.py
+++ bot.py
@@ -130,6 +130,26 @@ def tuer(update, context):
     logger.info("current day: " + str(day) + " " + "response text: " + response)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=item_url[day], caption=response)
 
+@bot.message_handler(commands=['oeffnen'])
+def oeffnen(update, context): 
+    text = "Welches Tuerchen willst du oeffnen?."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+def day_handler(message):
+    day = message.text
+    if day > len(responses):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Glückwunsch, du hast das Ende erreicht! Heute kein Knoblauch für dich. Aber dafür frohe Weihnachten!!")
+   
+    '''
+    Craft the response message text by getting text flair and item based on day
+    And get image to attach to message
+    '''
+    response = responses[day] + "..." + items[day] 
+    logger.info("current day: " + str(day) + " " + "response text: " + response)
+    context.bot.send_message(message.chat.id, response, parse_mode="Markdown")
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=item_url[day], caption=response)
+
 # linking the /random command with the function random() 
 day_handler = CommandHandler('tuer', tuer)
 dispatcher.add_handler(day_handler)
